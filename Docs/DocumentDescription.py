1901a71818d13266c52d88f6c7fcb125123a08d8
Romakhin Aleksandr <a.romakhin@cft.ru>
2020-10-31 15:27:12 +0700

    add some docs

--- Docs/DocumentDescription.py
+++ Docs/DocumentDescription.py
@@ -36,7 +36,7 @@ list_of_documents = [
         rus_name='Свидетельство об утверждении архитектурно-градостроительного решения',
         fields=[
             DocumentField(
-                name='',
+                name='department',
                 rus_name='Административный округ',
             ),
             DocumentField(
@@ -57,6 +57,125 @@ list_of_documents = [
             ),
         ]
     ),
+    DocumentDescription(
+        name='agreed_indicators',
+        rus_name='Согласованные технико-экономические показатели по объекту',
+        fields=[
+            DocumentField(
+                name='square_building',
+                rus_name='Площадь застройки',
+            ),
+            DocumentField(
+                name='volume',
+                rus_name='Объём',
+            ),
+            DocumentField(
+                name='number_storeys',
+                rus_name='Этажность',
+            ),
+            DocumentField(
+                name='top_indicator',
+                rus_name='Верхняя отметка объекта',
+            ),
+            DocumentField(
+                name='full_square',
+                rus_name='Общая площадь',
+            ),
+            DocumentField(
+                name='full_square_with_on_ground',
+                rus_name='В том числе надземная',
+            ),
+            DocumentField(
+                name='on_ground_square',
+                rus_name='Надземная',
+            ),
+        ]
+    ),
+    DocumentDescription(
+        name=' building_permission_capital',
+        rus_name='Разрешение на ввод Объекта капитального строительства',
+        fields=[
+            DocumentField(
+                name='address',
+                rus_name='Расположенного по адресу',
+            ),
+            DocumentField(
+                name='building_address',
+                rus_name='Строительный адрес',
+            ),
+            DocumentField(
+                name='volume_proj_fact',
+                rus_name='Строительный объем – всего (по проекту, фактически)',
+            ),
+            DocumentField(
+                name='volume_proj_fact_include_on_ground',
+                rus_name='В том числе надземной части (по проекту, фактически)',
+            ),
+            DocumentField(
+                name='full_square',
+                rus_name='Общая площадь - (по проекту, фактически)',
+            ),
+            DocumentField(
+                name='square_buildings_proj_fact',
+                rus_name='Площадь встроенно-пристроенных помещений (по проекту,фактически)',
+            ),
+            DocumentField(
+                name='count_buildings',
+                rus_name='Количество зданий (шт)',
+            ),
+        ]
+    ),
+    DocumentDescription(
+        name=' building_permission_capital',
+        rus_name='Разрешение на ввод Объекта капитального строительства',
+        fields=[
+            DocumentField(
+                name='objects_not_industry',
+                rus_name='Объекты непроизводственного назначения',
+            ),
+            DocumentField(
+                name='number_proj_fact',
+                rus_name='Количество мест (по проекту, фактически)',
+            ),
+            DocumentField(
+                name='number_visits_proj_fact',
+                rus_name='Количество посещений (по проекту, фактически)',
+            ),
+            DocumentField(
+                name='capacity_volume_proj_fact',
+                rus_name='Вместимость (по проекту, фактически)',
+            ),
+            DocumentField(
+                name='full_square_parking',
+                rus_name='Общая площадь подземной автостоянки (по проекту, фактически)',
+            ),
+            DocumentField(
+                name='square_admin_buildings_proj_fact',
+                rus_name='Общая площадь административных помещений (по проекту, фактически)',
+            ),
+            DocumentField(
+                name='square_culture_buildings_proj_fact',
+                rus_name='Общая площадь помещений культурно-досугового назначения (по проекту, фактически)',
+            ),
+            DocumentField(
+                name='count_buildings',
+                rus_name='Торговая площадь (по проекту, фактически)',
+            ),
+            DocumentField(
+                name='count_buildings',
+                rus_name='Этажность (по проекту, фактически)',
+            ),
+        ]
+    ),
 ]
 
 
+Объекты непроизводственного назначения
+Количество мест (по проекту, фактически)
+Количество посещений (по проекту, фактически)
+Вместимость (по проекту, фактически)
+Общая площадь подземной автостоянки (по проекту, фактически)
+Общая площадь административных помещений (по проекту, фактически)
+Общая площадь помещений культурно-досугового назначения (по проекту, фактически)
+Торговая площадь (по проекту, фактически)
+Этажность (по проекту, фактически)
