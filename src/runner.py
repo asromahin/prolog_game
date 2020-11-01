95ba8c1e8b3cb97cd86daf7b9f0b6a7ee4007b0f
asrom <asromahin@yandex.ru>
2020-11-01 14:29:33 +0700

    fix

--- src/runner.py
+++ src/runner.py
@@ -6,11 +6,11 @@ from flask import render_template, send_file
 from PIL import Image
 import os
 
-GLOBAL_QUERY = None
+#GLOBAL_QUERY = None
 
 def init_app(cls_model_path, credential_path, template_folder='prolog_game'):
-  global GLOBAL_QUERY
-  GLOBAL_QUERY = 'название документа'
+  #global GLOBAL_QUERY
+  #GLOBAL_QUERY = 'название документа'
   app = Flask(__name__, template_folder=template_folder)
 
   app.config['SECRET_KEY'] = 'kj'
@@ -19,26 +19,25 @@ def init_app(cls_model_path, credential_path, template_folder='prolog_game'):
 
   @app.route('/',methods = ['GET', 'POST'])
   def index():
-    global GLOBAL_QUERY
+    #global GLOBAL_QUERY
     if request.method == 'POST':
       img = request.files.get('img')
       text = request.form.get('text')
       print(text)
-      if text is not None:
-        global GLOBAL_QUERY
-        GLOBAL_QUERY = text
+      #if text is not None:
+        #global GLOBAL_QUERY
+        #GLOBAL_QUERY = text
       if img is not None:
-        global GLOBAL_QUERY
+        #global GLOBAL_QUERY
         filename = img.filename
         path = filename
         img.save(path)
         nim = Image.open(path)
         global pipeline
-        result = pipeline.predict(nim, query=GLOBAL_QUERY)
-        return render_template('index.html', uploaded_img_name=filename, result=result['ner_result'][0][0], global_query=GLOBAL_QUERY)
+        result = pipeline.predict(nim, query='тип документа')
+        return render_template('index.html', uploaded_img_name=filename, result=result['ner_result'][0][0], global_query=None)
       else:
-        global GLOBAL_QUERY
-        return render_template('index.html', uploaded_img_name=None, result=None, global_query=GLOBAL_QUERY)
+        return render_template('index.html', uploaded_img_name=None, result=None, global_query=None)
 
     elif request.method == 'GET':
       return render_template('index.html', uploaded_img_name=None, result=None, global_query=None)
